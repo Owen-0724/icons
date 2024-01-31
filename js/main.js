96cc733bd4544fdce53e141bc673c21d2025f09e
Owen-0724 <144960357+Owen-0724@users.noreply.github.com>
2024-01-31 00:40:56 -0500

    reset files

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-//The Window.console property returns a reference to the console object, which provides methods for logging information to the browser's console.
 console.log("JS file connected");
 
 const vectorGraphic = document.querySelector('#badge');
