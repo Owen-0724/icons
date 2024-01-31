762c329e3e7ae781b08219b4eee9a0fe132c201d
Owen-0724 <144960357+Owen-0724@users.noreply.github.com>
2024-01-31 01:42:32 -0500

    adjust svgs

--- js/main.js
+++ js/main.js
@@ -1,10 +1,1 @@
 console.log("JS file connected");
-
-const vectorGraphic = document.querySelector('#badge');
-
-function logThisId() {
-    console.log(this);
-    console.log('clicked on this element', this.id);
-}
-
-vectorGraphic.addEventListener('click', logThisId);
